6aef69e4e012b41597689968376e51eac61018ea
Yorick_06 <unconfigured@null.spigotmc.org>
2025-03-09 17:04:05 +0100

    Version 2.0.0 - moved to the entrypoint system

--- src/main/java/cz/yorick/TomlResources.java
+++ src/main/java/cz/yorick/TomlResources.java
@@ -3,38 +3,42 @@ package cz.yorick;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.dataformat.toml.TomlFactory;
 import com.fasterxml.jackson.dataformat.toml.TomlMapper;
-import cz.yorick.api.resources.SimpleResources;
+import com.mojang.serialization.DynamicOps;
+import cz.yorick.api.FileTypeInitializer;
 import cz.yorick.ops.JacksonOps;
-import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.Reader;
 import java.io.Writer;
 
-public class TomlResources implements ModInitializer {
+public class TomlResources implements FileTypeInitializer<JsonNode> {
 	public static final String MOD_ID = "toml-resources";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 	public static final TomlFactory FACTORY = new TomlFactory();
 	public static final TomlMapper MAPPER = new TomlMapper();
 
 	static {
-		SimpleResources.registerOps("toml", JacksonOps.INSTANCE, TomlResources::readToml, TomlResources::writeToml);
 		LOGGER.info("Toml resources are now available.");
 	}
 
 	@Override
-	public void onInitialize() {
-	}
-
-	private static JsonNode readToml(Reader reader) throws Exception {
+	public JsonNode read(Reader reader) throws Exception {
 		return MAPPER.readTree(reader);
 	}
 
-	private static void writeToml(Writer writer, JsonNode node) throws Exception {
+	@Override
+	public void write(Writer writer, JsonNode node) throws Exception {
 		MAPPER.writeTree(FACTORY.createGenerator(writer), node);
 	}
 
-	public static void ensureRegistered() {
+	@Override
+	public String getExtension() {
+		return "toml";
+	}
+
+	@Override
+	public DynamicOps<JsonNode> getOps() {
+		return JacksonOps.INSTANCE;
 	}
 }
